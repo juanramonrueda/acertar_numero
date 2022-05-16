e96a1375c8c871a10fd5bf33af83d8195b3258ee
Juan Ramón Rueda Lao <105500270+juanramonrueda@users.noreply.github.com>
2022-05-16 08:58:49 +0200

    Update acertar_numero.py

--- acertar_numero.py
+++ acertar_numero.py
@@ -1,46 +1,36 @@
-print("Programa para jugar a acertar el número\n")
-
 #-------------------------------------------------------------------------------------------------------------------------------------
 #Importación de la biblioteca / módulo para generar variables pseudo-aleatorias
 import random
 
-#Importación de la biblioteca / módulo para realizar paradas momentaneas
+#Importación de la biblioteca /módulo para realizar paradas momentaneas
 import time
 
 #Importación de la biblioteca / módulo para poder realizar limpiezas de terminal
 import os
 
 #-------------------------------------------------------------------------------------------------------------------------------------
-#Inicialización de variables
-opcn_usuario = 0
-contador_opciones = 0
-limpiar_pantalla = str("n") #Para que por defecto no haga la limpieza del terminal o consola
-
-#-------------------------------------------------------------------------------------------------------------------------------------
 #Función para informar de las opciones
-def opciones_jugar():
+def mostrar_menu():
     print("1.- Para jugar desde el rango del 1 al 100\n")
     print("2.- Para establecer rangos personalizados\n")
     print("3.- Finalizar la ejecución del programa\n")
 
 #-------------------------------------------------------------------------------------------------------------------------------------
-#Función para limpiar el terminal o consola, codigo copiado vilmente de una web y modificado para realizar pregunta
-#Principalmente es para Linux y sistemas operativos basados en MS-DOS
+#Función para limpiar el terminal o consola
 def limpiar():
-    if contador_opciones >= 1:
-        limpiar_pantalla = str(input("¿Quiere limpiar la pantalla? (s/n)\n"))
-        if limpiar_pantalla == "s":
-            command = 'clear'
-            if os.name in ('nt', 'dos'):
-                command = 'cls'
-            os.system(command)
+    limpiar_pantalla = str(input("¿Quiere limpiar la pantalla? (s/n)\n"))
+    if limpiar_pantalla == "s":
+        command = 'clear'
+        if os.name in ('nt', 'dos'):
+            command = 'cls'
+        os.system(command)
 
 #-------------------------------------------------------------------------------------------------------------------------------------
 #Función para generar el número aleatorio desde el 1 al 100
 def aleatorio_defecto():
     contador_intentos = 0
-    aleatorio_generado = int(random.randint(1, 100))
-    numero = int(-1) #Hago que esta variable provoque la entrada al bucle siguiente
+    aleatorio_generado = random.randint(1, 100)    
+    numero = -1 #Hago que esta variable provoque la entrada al bucle siguiente
     while numero != aleatorio_generado:
         numero = int(input("Introduzca un número\n"))
         if numero < aleatorio_generado:
@@ -58,15 +48,22 @@ def aleatorio_defecto():
 #Función para generar un número aleatorio desde un rango personalizado
 def aleatorio_personalizado():
     contador_intentos = 0
-    primer_rango= int(input("Introduzca el primer rango\n"))
+    cota_inferior = int(input("Introduzca la cota inferior\n"))
     print("")
-    segundo_rango = int(input("Introduzca el segundo rango\n"))
+    cota_superior = int(input("Introduzca la cota superior\n"))
     print("")
-    if primer_rango < segundo_rango:
-        aleatorio_generado = int(random.randint(primer_rango, segundo_rango))
-    else:
-        aleatorio_generado = int(random.randint(segundo_rango, primer_rango))
-    numero = int(-999999999999999999999999999999999) #Provoco que entre al bucle con un número improbable
+
+    # Si la cota inferior es mayor que la cota superior intercambiamos los valores
+    if cota_inferior > cota_superior:
+        aux = cota_inferior
+        cota_inferior = cota_superior
+        cota_superior = aux
+    
+    # Generamos un número aleatorio dentro del rango
+    aleatorio_generado = random.randint(cota_inferior, cota_superior)
+
+    # Incializamos el número con un número que está fuera de rango
+    numero = cota_inferior - 1
     while numero != aleatorio_generado:
         numero = int(input("Introduzca un número\n"))
         if numero < aleatorio_generado:
@@ -81,18 +78,31 @@ def aleatorio_personalizado():
         time.sleep(4)
 
 #-------------------------------------------------------------------------------------------------------------------------------------
-#Código inicial y esencial para que el programa realice todas las instrucciones
-while opcn_usuario != "3":
-    limpiar()
-    contador_opciones = contador_opciones + 1 #Contador para que después de la primera iteración pregunte por la función de limpiar
-    opciones_jugar()
-    opcn_usuario = str(input("Introduzca la opción deseada\n"))
-    if opcn_usuario == "1":
-        aleatorio_defecto()
-    elif opcn_usuario == "2":
-        aleatorio_personalizado()
-    elif opcn_usuario == "3":
-        print("Programa finalizado")
-    else:
-        print("Se ha equivocado al introducir las opciones, son:")
-        opciones_jugar()
+# Programa Principal
+def main():
+    print("Programa para jugar a acertar el número\n")
+
+    #Inicialización de variables
+    opcn_usuario = 0
+
+    #Código inicial y esencial para que el programa realice todas las instrucciones
+    while opcn_usuario != 3:
+        mostrar_menu()
+        opcn_usuario = int(input("Introduzca la opción deseada\n"))
+        while opcn_usuario < 1 or opcn_usuario > 3:
+            print("La opción no es correcta. Seleccione un valor entre 1 y 3")
+            opcn_usuario = int(input("Introduzca la opción deseada\n"))
+
+        if opcn_usuario == 1:
+            aleatorio_defecto()
+        elif opcn_usuario == 2:
+            aleatorio_personalizado()
+        else:
+            print("Programa finalizado")
+
+        if opcn_usuario != 3:
+            limpiar()
+
+#-------------------------------------------------------------------------------------------------------------------------------------
+if __name__ == "__main__":
+    main()
